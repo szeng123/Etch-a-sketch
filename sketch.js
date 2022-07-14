d245e6d2d0a0a89ba5d4798700cc1141ad5e0951
Stanley Z <szeng135@outlook.com>
2022-07-13 23:43:03 -0400

    Change from inline block to CSS grid format

--- sketch.js
+++ sketch.js
@@ -1,24 +1,56 @@
+const container = document.getElementById('grid-space');
+
 function grid(dimension) {
-    const container = document.getElementById('grid');
-    for (i = 0; i < dimension; i++) {
-        //create new row elements for each iteration 'i' and give class/id to elements 
-        let row = document.createElement('div');
-        row.className = 'row';
-        row.id = 'row' + i;
+    //Create CSS variables to overwrite old value in CSS files (2) 
+    container.style.setProperty('--grid-rows', dimension);
+    container.style.setProperty('--grid-columns', dimension);
 
-        //append container with new child row elements
-        container.appendChild(row);
+    //Generate X number of grids
+    for (i = 0; i < (dimension * dimension); i++) {
+        let cell = document.createElement("div");
+        cell.innerText = (i + 1);
+        container.appendChild(cell);
+        cell.classList.add("grid-item");
+    };
+};
 
-        for (j = 0; j < dimension; j++) {
-            //define new element to create columns to form 'grid/box/cells/square' appearance of divs
-            let box = document.createElement('div');
-            box.textContent = j;
-            box.className = 'box';
-            box.id = 'box' + j;
-            //append box element to row elements
-            row.appendChild(box);
-        }
-    }
+function initialGrid() {
+    grid(16);
 }
 
-grid(6);
+// New grid button
+let reset = document.getElementById('reset');
+
+function newGridSize() {
+    while (true) {
+        let size = prompt('Please enter the new size of the grid (1-100).');
+        if ((typeof size == null)||(typeof size !== 'number')) {
+            console.log('Please enter a number! ');
+        } else if ((size < 1) || (size > 100)) {
+            console.log('Please enter a valid number! ');
+        } else {
+            console.log('valid');
+            break;
+        }
+        }
+
+}
+
+reset.addEventListener('click', newGridSize);
+
+
+initialGrid();
+
+
+
+
+//Change grid colors 
+let gridArray = document.getElementsByClassName('grid-item');
+
+let changeColor = function() {
+    event.target.classList.add('color');
+};
+
+for (k = 0; k < gridArray.length; k++) {
+    gridArray[k].addEventListener('mouseover', changeColor, false);
+}
